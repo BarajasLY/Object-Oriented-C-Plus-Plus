a6e5e9a1cbe01569b6fed4c1da7f61024f2fba86
BarajasLY <159092229+BarajasLY@users.noreply.github.com>
2024-02-05 18:06:00 -0800

    Create Hello World Demo

---
+++ Lab 1/Hello_World.cpp
@@ -0,0 +1,13 @@
+/***
+ * Hello Wolrd Demo
+***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
